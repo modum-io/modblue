c47618afbbc5554f46781908dd4e5631efed1ea7
Marco Crespi <marco.crespi@modum.io>
2020-11-03 20:20:23 +0100

    chore: Add transpiled code

--- lib/bindings/hci/gap.d.ts
+++ lib/bindings/hci/gap.d.ts
@@ -2,25 +2,15 @@
 import { EventEmitter } from 'events';
 import { AddressType } from '../../types';
 export declare interface Gap {
-	on(
-		event: 'discover',
-		listener: (
-			status: number,
-			address: string,
-			addressType: AddressType,
-			connectable: boolean,
-			advertisement: any,
-			rssi: number
-		) => void
-	): this;
+    on(event: 'discover', listener: (status: number, address: string, addressType: AddressType, connectable: boolean, advertisement: any, rssi: number) => void): this;
 }
 export declare class Gap extends EventEmitter {
-	private hci;
-	private scanState;
-	private scanFilterDuplicates;
-	private discoveries;
-	constructor(hci: any);
-	startScanning(allowDuplicates: boolean): Promise<void>;
-	stopScanning(): Promise<void>;
-	private onHciLeAdvertisingReport;
+    private hci;
+    private scanState;
+    private scanFilterDuplicates;
+    private discoveries;
+    constructor(hci: any);
+    startScanning(allowDuplicates: boolean): Promise<void>;
+    stopScanning(): Promise<void>;
+    private onHciLeAdvertisingReport;
 }
--- lib/bindings/hci/signaling.d.ts
+++ lib/bindings/hci/signaling.d.ts
@@ -1,9 +1,9 @@
 import { Hci } from './hci';
 export declare class Signaling {
-	private hci;
-	private handle;
-	constructor(hci: Hci, handle: number);
-	dispose(): void;
-	private onAclStreamData;
-	private processConnectionParameterUpdateRequest;
+    private hci;
+    private handle;
+    constructor(hci: Hci, handle: number);
+    dispose(): void;
+    private onAclStreamData;
+    private processConnectionParameterUpdateRequest;
 }
