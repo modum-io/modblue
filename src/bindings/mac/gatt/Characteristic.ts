97d6962b1d1be7a32f17d4d5f6a7e9ab32e34ee3
Marco Crespi <marco.crespi@modum.io>
2021-04-23 12:59:17 +0200

    fix(mac): Fix notifications

--- src/bindings/mac/gatt/Characteristic.ts
+++ src/bindings/mac/gatt/Characteristic.ts
@@ -9,9 +9,6 @@ export class MacGattCharacteristic extends GattCharacteristic {
 	public discoverDescriptors(): Promise<GattDescriptor[]> {
 		const noble = this.service.gatt.peripheral.adapter.noble;
 
-		this.descriptors.clear();
-		noble.discoverDescriptors(this.service.gatt.peripheral.uuid, this.service.uuid, this.uuid);
-
 		return new Promise<GattDescriptor[]>((resolve) => {
 			const handler = (dev: string, srv: string, char: string, descUUIDs: string[]) => {
 				if (dev === this.service.gatt.peripheral.uuid && srv === this.service.uuid && char === this.uuid) {
@@ -23,13 +20,14 @@ export class MacGattCharacteristic extends GattCharacteristic {
 				}
 			};
 			noble.on('descriptorsDiscover', handler);
+
+			this.descriptors.clear();
+			noble.discoverDescriptors(this.service.gatt.peripheral.uuid, this.service.uuid, this.uuid);
 		});
 	}
 
 	public read(): Promise<Buffer> {
 		const noble = this.service.gatt.peripheral.adapter.noble;
-
-		noble.read(this.service.gatt.peripheral.uuid, this.service.uuid, this.uuid);
 
 		return new Promise<Buffer>((resolve) => {
 			const handler = (dev: string, srv: string, char: string, data: Buffer, isNotification: boolean) => {
@@ -44,13 +42,13 @@ export class MacGattCharacteristic extends GattCharacteristic {
 				}
 			};
 			noble.on('read', handler);
+
+			noble.read(this.service.gatt.peripheral.uuid, this.service.uuid, this.uuid);
 		});
 	}
 
 	public write(value: Buffer): Promise<void> {
 		const noble = this.service.gatt.peripheral.adapter.noble;
-
-		noble.write(this.service.gatt.peripheral.uuid, this.service.uuid, this.uuid, value);
 
 		return new Promise<void>((resolve) => {
 			const handler = (dev: string, srv: string, char: string) => {
@@ -60,6 +58,8 @@ export class MacGattCharacteristic extends GattCharacteristic {
 				}
 			};
 			noble.on('write', handler);
+
+			noble.write(this.service.gatt.peripheral.uuid, this.service.uuid, this.uuid, value);
 		});
 	}
 
@@ -70,8 +70,6 @@ export class MacGattCharacteristic extends GattCharacteristic {
 	public notify(notify: boolean): Promise<void> {
 		const noble = this.service.gatt.peripheral.adapter.noble;
 
-		noble.notify(this.service.gatt.peripheral.uuid, this.service.uuid, this.uuid, notify);
-
 		return new Promise<void>((resolve) => {
 			const handler = (dev: string, srv: string, char: string) => {
 				if (dev === this.service.gatt.peripheral.uuid && srv === this.service.uuid && char === this.uuid) {
@@ -80,6 +78,8 @@ export class MacGattCharacteristic extends GattCharacteristic {
 				}
 			};
 			noble.on('notify', handler);
+
+			noble.notify(this.service.gatt.peripheral.uuid, this.service.uuid, this.uuid, notify);
 		});
 	}
 
